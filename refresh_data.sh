131badd29b884510c077f6aefff1141e8ed8b8c8
Martin Urbanec <martin.urbanec@wikimedia.cz>
2021-11-21 23:07:58 +0000

    refresh_data: Clean data before running the notebook

--- refresh_data.sh
+++ refresh_data.sh
@@ -3,4 +3,5 @@
 source conda-activate-stacked 2021-04-07T23.21.00_urbanecm
 
 cd '/home/urbanecm/Documents/steward/2021-wikimedia-humans-per-group'
+rm -f data/*.json
 jupyter nbconvert --to html --execute humans_by_group.ipynb
